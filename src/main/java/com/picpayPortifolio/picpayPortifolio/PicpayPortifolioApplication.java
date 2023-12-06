d504b2e0e9b28e7cf8df2b0bedf36c36ba11c96f
Igor Alves <igor193@live.com>
2023-12-06 16:46:40 -0300

    feat: initial config

---
+++ src/main/java/com/picpayPortifolio/picpayPortifolio/PicpayPortifolioApplication.java
@@ -0,0 +1,13 @@
+package com.picpayPortifolio.picpayPortifolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayPortifolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayPortifolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/picpayPortifolio/picpayPortifolio/PicpayPortifolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpayPortifolio.picpayPortifolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayPortifolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
